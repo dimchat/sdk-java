77c21cbbcbd265f51f04d0711abd86305325bc80
Moky <albert.moky@gmail.com>
2021-09-07 15:50:12 +0800

    simplify TCPGate

--- StarGate/src/main/java/chat/dim/stargate/TCPGate.java
+++ StarGate/src/main/java/chat/dim/stargate/TCPGate.java
@@ -33,144 +33,39 @@ package chat.dim.stargate;
 import java.io.IOException;
 import java.net.SocketAddress;
 
+import chat.dim.net.BaseHub;
 import chat.dim.net.Connection;
-import chat.dim.net.ConnectionState;
-import chat.dim.startrek.Docker;
+import chat.dim.port.Arrival;
+import chat.dim.port.Departure;
 import chat.dim.startrek.StarGate;
-import chat.dim.type.ByteArray;
-import chat.dim.type.Data;
 
-public class TCPGate extends StarGate implements Connection.Delegate<byte[]> {
+public abstract class TCPGate<D extends Departure<A, I>, A extends Arrival<A, I>, I>
+        extends StarGate<D, A, I> {
 
-    public final Connection<byte[]> connection;
+    private final BaseHub hub;
 
-    public TCPGate(Connection<byte[]> conn) {
-        super();
-        connection = conn;
+    public TCPGate(Delegate<D, A, I> delegate) {
+        super(delegate);
+        hub = createHub();
+    }
+
+    protected abstract BaseHub createHub();
+
+    @Override
+    public boolean process() {
+        hub.tick();
+        boolean available = hub.getActivatedCount() > 0;
+        boolean busy = super.process();
+        return available || busy;
     }
 
     @Override
-    protected Docker createDocker() {
-        // TODO: override to customize Worker
-        if (MTPDocker.check(this)) {
-            return new MTPDocker(this);
-        }
-        return null;
+    protected Connection getConnection(SocketAddress remote, SocketAddress local) {
+        return hub.getConnection(remote, local);
     }
 
     @Override
-    public boolean isRunning() {
-        // 1. StarGate not stopped
-        // 2. Connection not closed (or still have data unprocessed?)
-        return super.isRunning() && connection.isOpen();
-    }
-
-    @Override
-    public boolean isExpired() {
-        ConnectionState state = connection.getState();
-        return state != null && state.equals(ConnectionState.EXPIRED);
-    }
-
-    @Override
-    public Status getStatus() {
-        return getStatus(connection.getState());
-    }
-
-    //
-    //  Connection Status -> Gate Status
-    //
-    public static Status getStatus(ConnectionState state) {
-        String name = state == null ? ConnectionState.DEFAULT : state.name;
-        switch (name) {
-            case ConnectionState.CONNECTING:
-                return Status.CONNECTING;
-            case ConnectionState.CONNECTED:
-            case ConnectionState.MAINTAINING:
-            case ConnectionState.EXPIRED:
-                return Status.CONNECTED;
-            case ConnectionState.ERROR:
-                return Status.ERROR;
-            default:
-                return Status.INIT;
-        }
-    }
-
-    @Override
-    public boolean send(byte[] pack) {
-        if (!connection.isOpen() || !connection.isConnected()) {
-            return false;
-        }
-        SocketAddress remote = connection.getRemoteAddress();
-        try {
-            return connection.send(pack, remote) != -1;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    @Override
-    public byte[] receive(int length, boolean remove) {
-        ByteArray fragment = receive(length);
-        if (fragment == null) {
-            return null;
-        } else if (fragment.getSize() > length) {
-            if (remove) {
-                // fragment[length:]
-                chunks = fragment.slice(length);
-            }
-            // fragment[:length]
-            fragment = fragment.slice(0, length);
-        } else if (remove) {
-            //assert fragment.length == length : "fragment length error";
-            chunks = null;
-        }
-        return fragment.getBytes();
-    }
-
-    private ByteArray receive(int length) {
-        int prev, next = chunks == null ? 0 : chunks.getSize();
-        do {
-            prev = next;
-            if (prev >= length) {
-                // received data size is big enough
-                break;
-            }
-            // try to receive data from connection
-            connection.tick();
-            // get next received data size
-            next = chunks == null ? 0 : chunks.getSize();
-        } while (next > prev);
-        return chunks;
-    }
-
-    private ByteArray chunks = null;
-
-    //
-    //  ConnectionHandler
-    //
-
-    @Override
-    public void onConnectionStateChanged(Connection connection, ConnectionState previous, ConnectionState current) {
-        Status s1 = getStatus(previous);
-        Status s2 = getStatus(current);
-        if (!s1.equals(s2)) {
-            Delegate delegate = getDelegate();
-            if (delegate != null) {
-                delegate.onStatusChanged(this, s1, s2);
-            }
-        }
-    }
-
-    @Override
-    public void onConnectionDataReceived(Connection<byte[]> connection, SocketAddress remote, byte[] pack) {
-        if (pack != null && pack.length > 0) {
-            // append data
-            if (chunks == null) {
-                chunks = new Data(pack);
-            } else {
-                chunks = chunks.concat(pack);
-            }
-        }
+    protected Connection connect(SocketAddress remote, SocketAddress local) throws IOException {
+        return hub.connect(remote, local);
     }
 }
